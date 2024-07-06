5b884440cdd197ee2ccd8aef66b5ab4045797be7
Jacob North <34386437+North15@users.noreply.github.com>
2024-07-06 03:16:20 -0400

    minor tweaks

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const darkModeToggle = document.getElementById("darkModeToggle");
-    const body = document.body;
-
-    darkModeToggle.addEventListener("click", () => {
-        body.classList.toggle("dark-mode");
-    });
-
     const articlesPerPage = 12;
     let currentPage = 1;
     let articles = [];
